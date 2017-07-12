5f1755c0b16657a3708caeda2a803ab6376ac5a3
jrqc <gurbuz.demiroglu@gmail.com>
2017-07-12 23:07:03 +0300

    test comment in

--- newtest.py
+++ newtest.py
@@ -3,31 +3,28 @@ from models import *
 kwargs = {'age': 10, 'name': 'namenew'}
 a = RDB({'age': 1, 'name': 'name'})
 
-# print a
-# print a['age']
-# print a['name']
-# print a.age
-# print a.name
-# print getattr(a, 'age')
-# print getattr(a, 'name')
-# print a.keys()
-# print a.items()
-# print a.values()
-# print a.copy()
-# a.clear()
-# print a.copy()
-# print a
+print a
+print a['age']
+print a['name']
+print a.age
+print a.name
+print getattr(a, 'age')
+print getattr(a, 'name')
+print a.keys()
+print a.items()
+print a.values()
+a.clear()
+print a
 
 
 
-# RDB.create(**kwargs)
-# RDB.create(kwargs)
-#
-#
-# RDB.lists()
+RDB.create(**kwargs)
+RDB.create(kwargs)
+
+
+RDB.lists()
 
 c = RDB.list(query={
-    "list": "test_2",
     "count": 100,
     "page": 0,
     "where": [],
